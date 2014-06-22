86e89e3dfb782e63273370db4af7cfa551c838a9
freyr@diybdsm.com <freyr@diybdsm.com>
2014-06-22 11:10:23 -0400

    last checking for sails... moving to microscope

--- commands/index.js
+++ commands/index.js
@@ -10,12 +10,47 @@ module.exports = {
             done();
         });
     },
-    
-    pushTest: function(done) {
+    sendArousalNotification: function(done) {
+        var later = require('later');
         var moment = require('moment');
+        var PushBullet = require('pushbullet');
+        var pusher = new PushBullet('v1Mc0siAtzdNSpEpOxsy13LYqAxPciDKSAujvgJegjHUa');
+
+        later.date.localTime();
+        //var sched = later.parse.text('every 1 hour between 10 am and 10 pm');
+        //var sched = later.parse.recur().on(10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22).hour();
+        var sched = later.parse.text('every 1 min');
+        //console.log(later.schedule(sched).next(10));
+
+        var count = 12;
+        var t = later.setInterval(pushItGood, sched);
+
+        function pushItGood() {
+            
+            console.log("Sending checkin link...");
+            
+            var link = 'http://localhost:1337/arousal';
+
+            //var deviceIden = 'ujvgJegjHUadjxmRBQTeEu'; // iphone
+            var deviceIden = 'ujvgJegjHUadjAsoeMFET6'; // chrome
+            pusher.link(deviceIden, 'Time to check in...', link, function(err, res) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(res);
+                }
+                count--;
+                if (count <=0) {
+                    t.clear();
+                    done();
+                }
+            });
+        }
+    },
+    listDevices: function(done) {
         var PushBullet = require("pushbullet");
         var pusher = new PushBullet('v1Mc0siAtzdNSpEpOxsy13LYqAxPciDKSAujvgJegjHUa');
-        /*
+
         pusher.devices(function(err, res) {
             if (err) {
                 console.log(err);
@@ -24,17 +59,31 @@ module.exports = {
             }
             done();
         });
-        */
-       
-       var deviceIden = 'ujvgJegjHUadjxmRBQTeEu';
-       //pusher.note(deviceIden, 'This is a test', 'This is a test on ' + moment(), function(err, res) {
-       pusher.link(deviceIden, 'Time to start some chastity...', 'http://devel.diybdsm.com/days/chastity/', function(err, res){
-           if (err) {
-               console.log(err);
-           } else {
-               console.log(res);
-           }
-           done();
-       });
+    },
+    pushTest: function(done) {
+        var moment = require('moment');
+        var PushBullet = require("pushbullet");
+        var pusher = new PushBullet('v1Mc0siAtzdNSpEpOxsy13LYqAxPciDKSAujvgJegjHUa');
+        /*
+         pusher.devices(function(err, res) {
+         if (err) {
+         console.log(err);
+         } else {
+         console.log(res);
+         }
+         done();
+         });
+         */
+
+        var deviceIden = 'ujvgJegjHUadjxmRBQTeEu';
+        //pusher.note(deviceIden, 'This is a test', 'This is a test on ' + moment(), function(err, res) {
+        pusher.link(deviceIden, 'Time to start some chastity...', 'http://devel.diybdsm.com/days/chastity/', function(err, res) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(res);
+            }
+            done();
+        });
     }
 };
---
+++ dbtest2.js
@@ -0,0 +1,7 @@
+var Waterline = require('waterline');
+
+console.log(Waterline);
+
+// Instantiate a new instance of the ORM
+var orm = new Waterline();
+console.log(orm);
